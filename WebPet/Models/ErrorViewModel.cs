da3c114321ad2af8027dc8a3a141ccfd0aa2a730
ThanhCuD <thanhduc552018@gmail.com>
2020-02-17 22:20:35 +0700

    first commit

---
+++ WebPet/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebPet.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
