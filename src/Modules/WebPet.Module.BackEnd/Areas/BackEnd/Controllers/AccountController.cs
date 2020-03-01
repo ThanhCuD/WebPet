16498ce7dd12b7afd3a722eba8aa5cba24fd1c68
ThanhCuD <thanhduc552018@gmail.com>
2020-03-01 19:34:10 +0700

    add

---
+++ src/Modules/WebPet.Module.BackEnd/Areas/BackEnd/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebPet.Module.BackEnd.Areas.BackEnd.Controllers
+{
+    [Area("BackEnd")]
+    public class AccountController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/Modules/WebPet.Module.BackEnd/Areas/BackEnd/ViewModels/LoginViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebPet.Module.BackEnd.Areas.BackEnd.ViewModels
+{
+    public class LoginViewModel
+    {
+    }
+}
